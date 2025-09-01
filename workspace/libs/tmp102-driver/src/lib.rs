487116df20c640fe8945af5ac1348841aff09b56
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2025-09-01 12:21:29 -0600

    Added unit tests to tmp102 library

--- workspace/libs/tmp102-driver/src/lib.rs
+++ workspace/libs/tmp102-driver/src/lib.rs
@@ -82,3 +82,127 @@ where
         (temp_signed as f32) * 0.0625
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    // Import top-level structs/functions
+    use super::*;
+
+    // Explicitly link to std
+    extern crate std;
+
+    // Test-only imports
+    use embedded_hal::i2c::{Error as I2cError, ErrorKind, Operation};
+
+    // I2C stub
+    #[derive(Debug)]
+    pub struct I2cStub {
+        pub response_data: [u8; 2],
+        pub call_count: usize,
+    }
+
+    // I2C bus with temperature sensor stub implementation
+    impl I2cStub {
+
+        // Create a new I2C bus
+        pub fn new() -> Self {
+            Self {
+                response_data: [0x00, 0x00],
+                call_count: 0,
+            }
+        }
+
+        // Set temperature (in Celsius)
+        pub fn set_temperature(&mut self, temp_c: f32) {
+            // Convert temperature to sensor format
+            let temp_raw = ((temp_c / 0.0625) as i16) << 4;
+            self.response_data[0] = (temp_raw >> 8) as u8;
+            self.response_data[1] = (temp_raw & 0xFF) as u8;
+        }
+    }
+
+    // Declare a dummy error type
+    #[derive(Debug, Clone)]
+    pub struct DummyError;
+
+    // Implement I2cError trait for the DummyError type
+    impl I2cError for DummyError {
+       fn kind(&self) -> ErrorKind {
+        ErrorKind::Other
+       }
+    }
+
+    // Associated type: use our dummy error type (for e.g. Self::Error)
+    impl embedded_hal::i2c::ErrorType for I2cStub {
+        type Error = DummyError;
+    }
+
+    // Stub mplementations of the basic I2C read/write functions
+    impl embedded_hal::i2c::I2c for I2cStub {
+
+        // Always return Ok
+        fn read(
+            &mut self, 
+            _address: u8, 
+            _read: &mut [u8]
+        ) -> Result<(), Self::Error> {
+            Ok(())
+        }
+
+        // Always return Ok
+        fn write(
+            &mut self, 
+            _address: u8, 
+            _write: &[u8]
+        ) -> Result<(), Self::Error> {
+            Ok(())
+        }
+
+        // Always return Ok
+        fn write_read(
+            &mut self,
+            _address: u8,
+            _write: &[u8],
+            read: &mut [u8],
+        ) -> Result<(), Self::Error> {
+            // Return canned response
+            read.copy_from_slice(&self.response_data);
+            self.call_count += 1;
+            Ok(())
+        }
+
+        // Always return Ok
+        fn transaction(
+            &mut self,
+            _address: u8,
+            _operations: &mut [Operation<'_>],
+        ) -> Result<(), Self::Error> {
+            Ok(())
+        }
+    }
+
+    // Unit test 1: create a new driver and make sure the device address is set
+    #[test]
+    fn test_new_driver() {
+        let i2c = I2cStub::new();
+        let driver = TMP102::new(i2c, Address::Ground);
+        assert_eq!(driver.address.as_u8(), 0x48);
+    }
+
+    // Unit test 2: Read the temperature using the I2C stub driver
+    #[test]
+    fn test_temperature_read() {
+        // Create a new I2C stub driver
+        let mut i2c = I2cStub::new();
+
+        // Set the temperature
+        i2c.set_temperature(25.0);
+
+        // Read the temperature
+        let mut driver = TMP102::new(i2c, Address::Ground);
+        let temp = driver.read_temperature_c().unwrap();
+
+        assert_eq!(temp, 25.0);
+    }
+}
